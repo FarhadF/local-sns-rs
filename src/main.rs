99d051b7a8a04f59873a9d8c6017901d7b43668f
Farhad Farahi <farhad@adjoe.io>
2026-01-25 23:05:00 +0100

    switch from serde-xml-rs to quicksxml;

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ use axum::response::{IntoResponse, Response};
 use axum::Router;
 use axum::routing::post;
 use dashmap::DashMap;
-use serde::{Deserialize, Serialize};
+use quick_xml::events::BytesText;
+use serde::Deserialize;
+use quick_xml::Writer;
+use std::io::Cursor;
 use std::net::SocketAddr;
 use std::sync::Arc;
 use tracing_subscriber;
@@ -50,30 +53,49 @@ struct SnsRequest {
 }
 
 // CreateTopic
-#[derive(Serialize, Debug)]
-#[serde(rename_all = "PascalCase")]
+#[derive(Debug)]
 struct CreateTopicResponse {
     create_topic_result: CreateTopicResult,
     response_metadata: ResponseMetadata,
 }
 
-#[derive(Serialize, Debug)]
-#[serde(rename_all = "PascalCase")]
+#[derive(Debug)]
 struct CreateTopicResult {
     topic_arn: String,
 }
 
-#[derive(Serialize, Debug)]
-#[serde(rename_all = "PascalCase")]
+#[derive(Debug)]
 struct ResponseMetadata {
     request_id: String,
 }
 
 // DeleteTopic
-#[derive(Serialize, Debug)]
-#[serde(rename_all = "PascalCase")]
+#[derive(Debug)]
 struct DeleteTopicResponse {
     response_metadata: ResponseMetadata,
+}
+
+// ListTopics
+#[derive(Debug)]
+struct ListTopicsResponse {
+    list_topics_result: ListTopicsResult,
+    response_metadata: ResponseMetadata,
+}
+
+#[derive(Debug, Default)]
+struct ListTopicsResult {
+    topics: Topics,
+    next_token: Option<String>,
+}
+
+#[derive(Debug, Default)]
+struct Topics {
+    member: Vec<Member>,
+}
+
+#[derive(Debug)]
+struct Member {
+    topic_arn: String,
 }
 
 
@@ -102,6 +124,7 @@ async fn handle_aws_request(
     match params.action.as_str() {
         "CreateTopic" => create_topic(State(state), params).await,
         "DeleteTopic" => delete_topic(State(state), params).await,
+        "ListTopics" => list_topics(State(state)).await,
         _ => "Action not supported".into_response(),
     }
 }
@@ -124,17 +147,29 @@ async fn create_topic(
     };
     state.topics.insert(name, topic);
 
-    let response = CreateTopicResponse {
-        create_topic_result: CreateTopicResult {
-            topic_arn: arn,
-        },
-        response_metadata: ResponseMetadata {
-            request_id: Uuid::new_v4().to_string(),
-        },
-    };
+    let mut writer = Writer::new(Cursor::new(Vec::new()));
+    writer.create_element("CreateTopicResponse")
+        .with_attribute(("xmlns", "https://sns.amazonaws.com/doc/2010-03-31/"))
+        .write_inner_content(|writer| {
+            writer.create_element("CreateTopicResult")
+                .write_inner_content(|writer| {
+                    writer.create_element("TopicArn").write_text_content(BytesText::new(&arn))?;
+                    Ok(())
+                })?;
+            writer.create_element("ResponseMetadata")
+                .write_inner_content(|writer| {
+                    writer.create_element("RequestId").write_text_content(BytesText::new(&Uuid::new_v4().to_string()))?;
+                    Ok(())
+                })?;
+            Ok(())
+        }).unwrap();
 
-    let xml_response = serde_xml_rs::to_string(&response).unwrap();
-    xml_response.into_response()
+
+    let xml_response = writer.into_inner().into_inner();
+    Response::builder()
+        .header("Content-Type", "application/xml")
+        .body(axum::body::Body::from(xml_response))
+        .unwrap()
 }
 
 async fn delete_topic(
@@ -150,12 +185,66 @@ async fn delete_topic(
     let topic_name = topic_arn.split(':').last().unwrap_or_default();
     state.topics.remove(topic_name);
 
-    let response = DeleteTopicResponse {
-        response_metadata: ResponseMetadata {
-            request_id: Uuid::new_v4().to_string(),
-        },
-    };
+    let mut writer = Writer::new(Cursor::new(Vec::new()));
+    writer.create_element("DeleteTopicResponse")
+        .with_attribute(("xmlns", "https://sns.amazonaws.com/doc/2010-03-31/"))
+        .write_inner_content(|writer| {
+            writer.create_element("ResponseMetadata")
+                .write_inner_content(|writer| {
+                    writer.create_element("RequestId").write_text_content(BytesText::new(&Uuid::new_v4().to_string()))?;
+                    Ok(())
+                })?;
+            Ok(())
+        }).unwrap();
 
-    let xml_response = serde_xml_rs::to_string(&response).unwrap();
-    xml_response.into_response()
+    let xml_response = writer.into_inner().into_inner();
+    Response::builder()
+        .header("Content-Type", "application/xml")
+        .body(axum::body::Body::from(xml_response))
+        .unwrap()
+}
+
+async fn list_topics(
+    State(state): State<SharedState>
+) -> Response {
+    let topics = state.topics
+        .iter()
+        .map(|topic_ref| Member {
+            topic_arn: topic_ref.value().arn.clone(),
+        })
+        .collect::<Vec<_>>();
+
+    let mut writer = Writer::new(Cursor::new(Vec::new()));
+    writer.create_element("ListTopicsResponse")
+        .with_attribute(("xmlns", "https://sns.amazonaws.com/doc/2010-03-31/"))
+        .write_inner_content(|writer| {
+            writer.create_element("ListTopicsResult")
+                .write_inner_content(|writer| {
+                    writer.create_element("Topics")
+                        .write_inner_content(|writer| {
+                            for topic in topics {
+                                writer.create_element("member")
+                                    .write_inner_content(|writer| {
+                                        writer.create_element("TopicArn").write_text_content(BytesText::new(&topic.topic_arn))?;
+                                        Ok(())
+                                    })?;
+                            }
+                            Ok(())
+                        })?;
+                    writer.create_element("NextToken").write_text_content(BytesText::new(""))?;
+                    Ok(())
+                })?;
+            writer.create_element("ResponseMetadata")
+                .write_inner_content(|writer| {
+                    writer.create_element("RequestId").write_text_content(BytesText::new(&Uuid::new_v4().to_string()))?;
+                    Ok(())
+                })?;
+            Ok(())
+        }).unwrap();
+
+    let xml_response = writer.into_inner().into_inner();
+    Response::builder()
+        .header("Content-Type", "application/xml")
+        .body(axum::body::Body::from(xml_response))
+        .unwrap()
 }
